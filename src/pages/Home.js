937e5a9af82f4c433297ed918525c6cfc9ce51e7
azkall <120769795+azkazkiya@users.noreply.github.com>
2023-11-22 18:35:58 +0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,16 @@
-// src/pages/Home.js
+// src/pages/LandingPage.js
 import React from 'react';
-import './Home.css';
+import './Home.css'; // Gantilah dengan nama file CSS yang sesuai
 import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
-
 import Typed from 'react-typed'; // Import react-typed library
 import { useSpring, animated } from 'react-spring';
 import { useInView } from 'react-intersection-observer';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
+import AboutHome from './AboutHome';
+import Gallery from './Gallery';
 
 function Home() {
-
   const [descriptionRef, descriptionInView] = useInView({
     triggerOnce: true,
   });
@@ -18,7 +20,6 @@ function Home() {
     to: { opacity: 1, transform: 'translateY(0)' },
     config: { duration: 1700 },
   });
-
 
   return (
     <div className="page">
@@ -31,26 +32,27 @@ function Home() {
         showCursor
         cursorChar="|"
         className="typed-text"
-        style={{
+      	style={{
           display: 'block', padding: '5rem', backgroundImage: 'linear-gradient(to bottom, #4F6F52, #92ad91)', 
           margin: '0', fontSize:'60px', color: '#ECE3CE',
         }}
       />
 
-    <animated.div
+      <animated.div
         ref={descriptionRef}
-        style={{ ...descriptionSpring,  margin: '20px 0', }}
+        style={{ ...descriptionSpring, margin: '20px 0' }}
         className="description"
       >
-        <p>
-        Welcome to my portfolio! I Am Azkiya and here you can dive into my journey from the last few years. 
+        <p style={{padding:'5px 75px'}}>
+          Welcome to my portfolio! Hi, I'm Azkal Azkiya Alfiandri. I am a student at Universitas Pendidikan Indonesia majoring in computer science education. I have a hobby of reading and listening to music. I am very interested in education and humanity. 
         </p>
-        <p>
-        Have a nice day!
-        </p>
+        <p>Have a nice day!</p>
       </animated.div>
 
-      
+
+      {/* Komponen About dan Gallery */}
+      <AboutHome />
+      <Gallery />
     </div>
   );
 }
